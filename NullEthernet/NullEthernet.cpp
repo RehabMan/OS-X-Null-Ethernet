96d2a51ae9af3f0224a433197e9c0b15e7d159bd
RehabMan <racerrehabman@gmail.com>
2014-05-01 16:53:56 -0700

    work around bug in Xcode 5.0.1

--- NullEthernet/NullEthernet.cpp
+++ NullEthernet/NullEthernet.cpp
@@ -17,6 +17,15 @@
 
 #include "NullEthernet.h"
 
+//REVIEW: avoids problem with Xcode 5.1.0 where -dead_strip eliminates these required symbols
+#include <libkern/OSKextLib.h>
+void* _org_rehabman_dontstrip_[] =
+{
+    (void*)&OSKextGetCurrentIdentifier,
+    (void*)&OSKextGetCurrentLoadTag,
+    (void*)&OSKextGetCurrentVersionString,
+};
+
 #pragma mark --- public methods ---
 
 OSDefineMetaClassAndStructors(org_rehabman_NullEthernet, IOEthernetController)
